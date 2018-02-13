0dd99efa5decd781c09a0c5f84945e6d75c83c9c
Hao Hsiang Song <haohsiang@uchicago.edu>
2018-02-13 12:28:26 -0600

    added chunking

--- processor.py
+++ processor.py
@@ -2,13 +2,15 @@ import pickle
 import parser
 import sys
 import treeNode as tn
-from time import sleep
+import shutil
+from time import sleep, time
 from multiprocessing import Pool, Process, Manager
 from os import walk, path, makedirs
 
 sys.setrecursionlimit(50000)
 dump_path = path.join('..', 'output')
 reddit_path = path.join("..", 'redditHtmlData')
+chunk_path = path.join("..", "chunk{}")
 processed = []
 output_data = {}
 
@@ -148,25 +150,75 @@ def load(filename):
 
 
 def parallel_process(raw):
-    tree = parser.parse(
-        path.join(reddit_path, raw)
+    return parser.parse(
+        raw
     )
-    output_data[raw] = tree
 
 
-def multi_core():
-    global output_data
-    manager = Manager()
-    output_data = manager.dict()
+def prepend_path(dir, file):
+    return path.join(
+        dir, file
+    )
+
+
+def multi_core(chunk_number):
+    # global output_data
+    # manager = Manager()
+    # output_data = manager.dict()
     pool = Pool(processes=4)
-    pool.map(parallel_process, list_raw_files())
+    dir = chunk_path.format(chunk_number)
+    files = list_files(dir)
+    files = [prepend_path(dir, file) for file in files]
+    results = pool.imap_unordered(
+        parallel_process,
+        files
+    )
+    output = []
+    pool.close()
+    pool.join()
+    for result in results:
+        if result is None: continue
+        output.append(result)
+    save(output, path.join(dump_path, "chunk{}.dat".format(chunk_number)))
 
-    save(output_data, path.join('output', "parallel.dat"))
+
+def chunk_files():
+    files = list_raw_files()
+    chunk = []
+    count = 0
+    for file in files:
+        if len(chunk) > len(files) / 10:
+            move_files(chunk, chunk_path.format(count))
+            count += 1
+            chunk = []
+        chunk.append(file)
+    move_files(chunk, chunk_path.format(count))
+
+
+def move_files(files, folder):
+    if not path.exists(folder):
+        makedirs(folder)
+    for file in files:
+        shutil.move(
+            path.join(
+                reddit_path, file
+            ),
+            path.join(
+                folder, file
+            )
+        )
 
 
 if __name__ == "__main__":
+    start = time()
     # pre_process()
     # process(skip_processed=False)
     # post_process()
-    multi_core()
+    multi_core(0)
+    # chunk_files()
+
+    print("execution time: {}s".format(
+        time() - start
+    ))
     pass
+
