4ea72c635127543a8769892a579f8fd5f8dc6a0e
Hao Mou <sora@Haos-MacBook-Pro.local>
2018-02-19 17:41:15 -0600

    bug fixes

--- movement_btw_snapshots.py
+++ movement_btw_snapshots.py
@@ -3,9 +3,17 @@ def create_newdict(output1, output2):
     #  outputs two dictionaries with key - item = cluster - user
     dict1 = {}
     dict2 = {}
-    for i in range(len(output1)):
+    numCluster1 = 0
+    numCluster2 = 0
+    for key, item in output1.items():
+        if item > numCluster1:
+            numCluster1 = item
+    for key, item in output2.items():
+        if item > numCluster2:
+            numCluster2 = item
+    for i in range(numCluster1 + 1):
         dict1[i] = []
-    for i in range(len(output2)):
+    for i in range(numCluster2 + 1):
         dict2[i] = []
     for key in output1:
         dict1[output1[key]].append(key)
@@ -23,12 +31,13 @@ def percentage_of(cluster_dict, numNewCluster, user_dict):
     res = {}
     for cluster in cluster_dict:
         numUsers = len(cluster_dict[cluster])
-        for i in range(numCluster):
+        for i in range(numNewCluster):
             res[cluster, i] = 0
         for user in cluster_dict[cluster]:
-            tmp[cluster, user_dict[user]] += 1
-        for i in range(numCluster):
-            tmp[cluster, i] /= numUsers
+            if user in user_dict:
+                res[cluster, user_dict[user]] += 1
+        for i in range(numNewCluster):
+            res[cluster, i] /= numUsers
     return res
 
 def print_(res, flag):
@@ -41,18 +50,21 @@ def print_(res, flag):
         string = "comes from"
         string2 = "in the older snapshot"
     for denom, numer in res:
-        print("{0:.2f}% of cluster {1} {2} cluster {3} {4}".format(
-            res[denom, numer] * 100, denom, string, numer, string2))
+        if res[denom, numer] > 0:
+            print("{0:.2f}% of cluster {1} {2} cluster {3} {4}".format(
+                res[denom, numer] * 100, denom, string, numer, string2))
 
 def print_old_to_new(res):
     print("Breakdown of dests of users in the older snapshot")
     print("-------------------------------------------------")
     print_(res, True)
+    print("")
 
 def print_new_from_old(res):
     print("Breakdown of srcs of users in the newer snapshot")
     print("------------------------------------------------")
     print_(res, False)
+    print("")
 
 ''' 
 Print functions added a typical use case looks as follows:
@@ -61,5 +73,5 @@ Assume we have the output of louvain algo of two consecutive snapshots
 
 cluster_dict1, cluster_dict2 = create_newdict(output1, output2)
 print_old_to_new(percentage_of(cluster_dict1, len(cluster_dict2), output2))
-print_new_from_old(percentage_of(cluster_dict2, len(cluster_dict1), output1)))
+print_new_from_old(percentage_of(cluster_dict2, len(cluster_dict1), output1))
 '''
